7bbd4a19cd66f4fa91b1285df1769fc9109c2386
sebaconti87 <sebaconti87@gmail.com>
2021-10-13 19:39:36 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} className="logo" alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="d-flex justify-content-end">
+            <Nav.Link href="#home">Nosotros</Nav.Link>
+            <Nav.Link href="#link">Productos</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
